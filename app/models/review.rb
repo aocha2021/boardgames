027ab9edb200dba9838e2b0cba0179ed833d47ed
aocha <abcdeman00@gmail.com>
2021-10-25 07:37:55 +0000

    boardgames-f

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :luck, presence: true
+  validates :design, presence: true
+  validates :easy, presence: true
+  validates :strategy, presence: true
+  validates :teamwork, presence: true
+
+
   belongs_to :user
   belongs_to :game
 end
